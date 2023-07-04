2dcba5ce7241c1dd9236c1fe219ebe692fe923a8
Larry Tapia <tapialarry1@gmail.com>
2023-07-03 23:45:25 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+//Remove main
+main.remove();
+
+//Creating variable for h1
+const newHeader = document.createElement("h1");
+
+//The variable should have an id called victory
+newHeader.id = "victory";
+
+//The text should be in the header
+newHeader.textContent = "Larry is the champion";
